040d07c3256bf23c51b9ffb1dc0080a99e615dfc
wubishet <wubishetwudu1624@gmail.com>
2026-02-12 23:58:31 +0300

    refactor token genaration to genarate token

--- utils/tokenGenerator.js
+++ utils/tokenGenerator.js
@@ -1,7 +1,43 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, { expiresIn: "6d" });
+const generateAccessToken = (user) => {
+  const { id, school_id, roles } = user;
+
+  if (!roles || roles.length === 0) {
+    throw new Error("User has no roles assigned");
+  }
+
+  return jwt.sign(
+    {
+      sub: id,
+      "https://hasura.io/jwt/claims": {
+        "x-hasura-user-id": id,
+        "x-hasura-default-role": roles[0],
+        "x-hasura-allowed-roles": roles,
+        "x-hasura-school-id": school_id
+      }
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "15m",
+      issuer: "school-platform",
+      audience: "hasura"
+    }
+  );
 };
 
-module.exports = generateToken;
+const generateRefreshToken = (id) => {
+  return jwt.sign(
+    { sub: id },
+    process.env.JWT_REFRESH_SECRET_KEY,
+    {
+      expiresIn: "7d",
+      issuer: "school-platform"
+    }
+  );
+};
+
+module.exports = {
+  generateAccessToken,
+  generateRefreshToken
+};
